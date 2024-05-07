36aaaf0e0a53316ac8aa0e691233878241ce552b
kulshav <vovakulsha99@gmail.com>
2024-05-07 22:09:19 +0500

    Created get_balance method

--- storage/crud.py
+++ storage/crud.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
-from storage.core import StorageManager
+from utils.validator import CategoryEnum
+from storage.core import StorageManager, StorageDataEnum
 
 
 class QueryManager(StorageManager):
+    """
+    Class for executing crud queries from storage
+    """
     def __init__(self):
         super().__init__()
 
@@ -11,6 +15,21 @@ class QueryManager(StorageManager):
         self, date: datetime, category: str, amount: float, desc: str
     ):
         self._insert_row([date, category, amount, desc])
+
+    def get_current_balance(self) -> float:
+        rows = self._get_data_dict()
+
+        total_income = 0.0
+        total_expense = 0.0
+
+        for row in rows:
+            if row[StorageDataEnum.category.value] == CategoryEnum.income.value:
+                total_income += float(row[StorageDataEnum.amount.value])
+
+            if row[StorageDataEnum.category.value] == CategoryEnum.expense.value:
+                total_expense += float(row[StorageDataEnum.amount.value])
+
+        return total_income - total_expense
 
 
 query = QueryManager()
