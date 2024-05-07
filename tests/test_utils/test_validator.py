b4c7dbebf29df7524b8343ab33acc3787b2b8863
kulshav <vovakulsha99@gmail.com>
2024-05-07 14:54:05 +0500

    Tests for input data validator

---
+++ tests/test_utils/test_validator.py
@@ -0,0 +1,42 @@
+import pytest
+
+from utils.validator import CategoryEnum, InputValidator
+from promts.promts import user_promt
+
+
+class TestValidator:
+    def test_category_enum(self):
+        assert CategoryEnum.expense.value == "Expense"
+        assert CategoryEnum.income.value == "Income"
+
+        assert isinstance(CategoryEnum.expense.value, str)
+        assert isinstance(CategoryEnum.income.value, str)
+
+    @pytest.mark.parametrize(("date", "expected_result"), [
+        ("20-05-05", user_promt.wrong_date_input_format()),
+        ("asd", user_promt.wrong_date_input_format()),
+        ("123", user_promt.wrong_date_input_format()),
+        ("2024-2024-2024", user_promt.wrong_date_input_format()),
+        ("2024-13-123", user_promt.wrong_date_input_format()),
+    ])
+    def test_input_wrong_date_validator(self, date, expected_result):
+        with pytest.raises(ValueError) as error:
+            InputValidator.validate_date_format(date)
+        assert str(error.value) == expected_result
+
+    @pytest.mark.parametrize(("category", "expected_result"), [
+        ("20-05-05", user_promt.wrong_category_format(CategoryEnum.income.value, CategoryEnum.expense.value)),
+        ("asd", user_promt.wrong_category_format(CategoryEnum.income.value, CategoryEnum.expense.value)),
+        ("123", user_promt.wrong_category_format(CategoryEnum.income.value, CategoryEnum.expense.value)),
+        ("2024-2024-2024", user_promt.wrong_category_format(CategoryEnum.income.value, CategoryEnum.expense.value)),
+        ("2024-13-123", user_promt.wrong_category_format(CategoryEnum.income.value, CategoryEnum.expense.value)),
+    ])
+    def test_input_wrong_category_validator(self, category, expected_result):
+        with pytest.raises(ValueError) as error:
+            InputValidator.validate_category(category)
+        assert str(error.value) == expected_result
+
+    def test_validate_date_category_success(self):
+        assert InputValidator.validate_date_format("2024-01-01") == "2024-01-01"
+        assert InputValidator.validate_category(CategoryEnum.income.value) == CategoryEnum.income.value
+        assert InputValidator.validate_category(CategoryEnum.expense.value) == CategoryEnum.expense.value
