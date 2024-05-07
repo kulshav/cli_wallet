a43ad1c72dd5ecbce6f20d469fb969b0d97a7412
kulshav <vovakulsha99@gmail.com>
2024-05-07 22:09:47 +0500

    Created balance command

--- cli/display.py
+++ cli/display.py
@@ -1,5 +1,8 @@
 import typer
 
+from storage.crud import query
+from promts.promts import user_promt
+
 display_app = typer.Typer(
     no_args_is_help=True,
     add_completion=False,
@@ -7,9 +10,21 @@ display_app = typer.Typer(
 )
 
 
-@display_app.command(name="balance", no_args_is_help=True, short_help="")
+@display_app.command(name="balance")
 def display_current_balance():
     """
     Shows current balance
     """
-    pass
+    current_balance = query.get_current_balance()
+
+    echo_msg = user_promt.ok_balance()
+
+    # Just for giggles
+    if current_balance < 0:
+        echo_msg = user_promt.negative_balance()
+
+    return typer.echo(
+        f"Balance: {current_balance}\n\n"
+        f"{echo_msg}"
+    )
+
