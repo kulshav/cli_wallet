974376b857d2e8cb93288fce016c08d036c24288
kulshav <vovakulsha99@gmail.com>
2024-05-07 14:00:54 +0500

    Added promts to user

---
+++ promts/promts.py
@@ -0,0 +1,49 @@
+
+
+class UserPromt:
+    @staticmethod
+    def ask_to_enter_date() -> str:
+        return "Enter the date in format (YYYY-MM-DD) Press Enter(today) ->"
+
+    @staticmethod
+    def ask_to_enter_category() -> str:
+        return "Enter the category - (Income | Expense) Press Enter ->"
+
+    @staticmethod
+    def ask_to_enter_amount() -> str:
+        return "Enter the amount"
+
+    @staticmethod
+    def ask_to_enter_desc() -> str:
+        return "Enter the description (Press Enter to skip)"
+
+    @staticmethod
+    def wrong_date_input_format() -> str:
+        return "Incorrect date format. The correct format is 'YYYY-MM-DD'\n\nUse the --help flag for assistance"
+
+    @staticmethod
+    def wrong_category_format(*args) -> str:
+        return f"Invalid category!\nAvailable categories: {args}\n\nUse the --help flag for assistance"
+
+    @staticmethod
+    def negative_amount_input() -> str:
+        return "You entered a negative amount.\n\nUse the --help flag for assistance"
+
+    @staticmethod
+    def input_not_number():
+        return "You did not enter a number!\nThe correct format is: 1000, 1000.00\n\nUse the --help flag for assistance"
+
+    @staticmethod
+    def successful_record_input(date, category, amount, desc) -> str:
+        return (
+            "\n\nYou have successfully entered a new record:\n"
+            f"Date: {date}\n"
+            f"Category: {category}\n"
+            f"Amount: {amount}\n"
+            f"Description: {desc}"
+        )
+
+
+user_promt = UserPromt()
+
+
