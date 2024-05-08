2b3f6636667c30de171fd99f4bb30c9272346a31
kulshav <vovakulsha99@gmail.com>
2024-05-08 21:56:54 +0500

    Small refactoring

--- tests/test_storage/test_storage_manager.py
+++ tests/test_storage/test_storage_manager.py
@@ -5,24 +5,24 @@ from storage.core import StorageManager, StorageDataEnum
 
 class TestStorageManager:
     def test_storage_data_enum(self):
-        assert StorageDataEnum.date.value == "Date"
-        assert isinstance(StorageDataEnum.date.value, str)
+        assert StorageDataEnum.date == "Date"
+        assert isinstance(StorageDataEnum.date, str)
 
-        assert StorageDataEnum.category.value == "Category"
-        assert isinstance(StorageDataEnum.category.value, str)
+        assert StorageDataEnum.category == "Category"
+        assert isinstance(StorageDataEnum.category, str)
 
-        assert StorageDataEnum.amount.value == "Amount"
-        assert isinstance(StorageDataEnum.amount.value, str)
+        assert StorageDataEnum.amount == "Amount"
+        assert isinstance(StorageDataEnum.amount, str)
 
-        assert StorageDataEnum.desc.value == "Description"
-        assert isinstance(StorageDataEnum.desc.value, str)
+        assert StorageDataEnum.desc == "Description"
+        assert isinstance(StorageDataEnum.desc, str)
 
     def test_storage_manager(self, storage_instance, restart_storage):
         headers = [
-            StorageDataEnum.date.value,
-            StorageDataEnum.category.value,
-            StorageDataEnum.amount.value,
-            StorageDataEnum.desc.value
+            StorageDataEnum.date,
+            StorageDataEnum.category,
+            StorageDataEnum.amount,
+            StorageDataEnum.desc
         ]
         expense_record = ["2024-05-07", "Expense", "500.0", "Groceries"]
         income_record = ["2024-05-07", "Income", "1000.0", "Scholarship"]
@@ -34,16 +34,16 @@ class TestStorageManager:
         assert storage_instance._get_data_list() == [headers, expense_record, income_record]
         assert storage_instance._get_data_dict() == [
             {
-                StorageDataEnum.amount.value: expense_record[2],
-                StorageDataEnum.category.value: expense_record[1],
-                StorageDataEnum.date.value: expense_record[0],
-                StorageDataEnum.desc.value: expense_record[3],
+                StorageDataEnum.amount: expense_record[2],
+                StorageDataEnum.category: expense_record[1],
+                StorageDataEnum.date: expense_record[0],
+                StorageDataEnum.desc: expense_record[3],
             },
             {
-                StorageDataEnum.amount.value: income_record[2],
-                StorageDataEnum.category.value: income_record[1],
-                StorageDataEnum.date.value: income_record[0],
-                StorageDataEnum.desc.value: income_record[3],
+                StorageDataEnum.amount: income_record[2],
+                StorageDataEnum.category: income_record[1],
+                StorageDataEnum.date: income_record[0],
+                StorageDataEnum.desc: income_record[3],
             },
         ]
 
