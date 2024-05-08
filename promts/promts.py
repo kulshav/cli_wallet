c778acc6d333e0e5ebcbe035cf99583e1cd26d41
kulshav <vovakulsha99@gmail.com>
2024-05-08 21:59:42 +0500

    Small refactoring. Added more promts

--- promts/promts.py
+++ promts/promts.py
@@ -50,8 +50,10 @@ class UserPromt:
 
     @staticmethod
     def negative_balance() -> str:
-        return ("Your balance is negative!\n"
-                "Maybe just spend less and earn more? It`s that simple")
+        return (
+            "Your balance is negative!\n"
+            "Maybe just spend less and earn more? It`s that simple"
+        )
 
     @staticmethod
     def ok_balance() -> str:
@@ -63,10 +65,74 @@ class UserPromt:
             "Impulsive buying is the only way to spend money. Source: Trust me bro.",
             "Don't bother with paying out loans. "
             "Ignorance is bliss, especially when creditors come knocking for your TV",
-            "Why worry about compound interest when you can just compound your debt instead?"
+            "Why worry about compound interest when you can just compound your debt instead?",
         ]
 
         return f"Very very informative financial tip: {random.choice(informative_tip)}"
+
+    @staticmethod
+    def not_found_any_records() -> str:
+        return "Not found any records. Maybe try harder? 😇"
+
+    @staticmethod
+    def ask_to_enter_year() -> str:
+        return "Input any year as a number. Default -> current"
+
+    @staticmethod
+    def ask_to_enter_month() -> str:
+        return "Input any month as a number. Default -> current"
+
+    @staticmethod
+    def ask_to_enter_day() -> str:
+        return "Input any day as a number. Default -> current"
+
+    @staticmethod
+    def negative_date() -> str:
+        return "You entered a negative date.\n\nUse the --help flag for assistance"
+
+    @staticmethod
+    def ask_to_enter_record_id() -> str:
+        return "Input record ID as number. Enter to ->"
+
+    @staticmethod
+    def wrong_input_record_id() -> str:
+        return "Record with entered ID doesn`t exists"
+
+    @staticmethod
+    def check_record_to_edit() -> str:
+        return "Please check record before editing (y/n to continue)"
+
+    @staticmethod
+    def ask_to_enter_date_options() -> str:
+        return "Input date. Enter to set unchanged ->"
+
+    @staticmethod
+    def ask_to_enter_category_options() -> str:
+        return "Input category (Income, Expense). Enter to set unchanged ->"
+
+    @staticmethod
+    def ask_to_enter_amount_options() -> str:
+        return "Input amount. Enter to set unchanged ->"
+
+    @staticmethod
+    def ask_to_enter_desc_options() -> str:
+        return "Input description. Enter to set unchanged ->"
+
+    @staticmethod
+    def record_output_with_id(
+        record_id: int,
+        date: str,
+        category: str,
+        amount: str,
+        desc: str,
+    ):
+        return (
+            f"\nID: {record_id}\n\n"
+            f"Date: {date}\n"
+            f"Category: {category}\n"
+            f"Amount: {amount}\n"
+            f"Description: {desc}\n"
+        )
 
 
 user_promt = UserPromt()
