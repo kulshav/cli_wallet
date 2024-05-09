b00d10c605308822cc1e83515edb6a1e6fa4171e
kulshav <vovakulsha99@gmail.com>
2024-05-09 19:27:04 +0500

    Minor updates

--- configs/config.py
+++ configs/config.py
@@ -4,14 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    TEST_MODE: bool = True  # Default = False on production
-
-    env_file: str = ".test.env" if TEST_MODE else ".env"
-    env_file_path: str = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "..", env_file)
-    )
-
-    model_config = SettingsConfigDict(env_file=env_file_path, extra="ignore")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     PATH_TO_STORAGE: str
 
